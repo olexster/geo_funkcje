6086eddf158905288acc79300165a5654c7250f2
olexster <olexster@gmail.com>
2020-04-01 06:11:51 +0200

    początek

--- funkcje.py
+++ funkcje.py
@@ -1,3 +1,14 @@
-def sr_okr(xy){
-    is xy
-}
+import numpy
+
+def sr_okr(xy):
+    try:
+        xy = numpy.array(xy,dtype=float)
+
+        print(xy3)
+
+    except ValueError as err:
+        print('wystąpił błąd: ',err, xy)
+
+
+#sr_okr([[1,2],['a',2]])
+sr_okr([[1,2],[3,3],[4,6],[1,5],[2,6],[7,8]])
