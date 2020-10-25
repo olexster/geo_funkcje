e81ce7be2768fa4137dc90209d25610b92de4728
Szymon <olexster@gmail.com>
2020-10-25 19:26:38 +0100

    baza danych

--- venv/Lib/baza.py
+++ venv/Lib/baza.py
@@ -2,7 +2,8 @@ import os
 
 from datetime import date
 from peewee import *
-def  nowaBaza(file = "baza_" + str(date.today())):  # todo: domyślan nazawa pliku "data i godzina"
+def  nowaBaza(file = "baza_" + str(date.today())):  # TODO
+    # : domyślan nazawa pliku "data i godzina"
     # todo: zmienna godło
   file = file+".db"
     if os.path.exists(file):
